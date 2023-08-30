0f9ac874a7bac489040bfe692b0c789d2dbac8d7
CoffeeRobotGH <emanuelparedesokorov@gmail.com>
2023-08-30 15:35:04 -0300

    commit

---
+++ .NET/PeliculasAPI/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using PeliculasAPI.DTOs;
+using PeliculasAPI.Entidades;
+
+namespace PeliculasAPI.Helpers
+{
+    public class AutoMapperProfiles: Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Genero, GeneroDTO>().ReverseMap();
+            CreateMap<GeneroCreacionDTO, Genero>();
+
+            CreateMap<Actor, ActorDTO>().ReverseMap();
+            CreateMap<ActorCreacionDTO, Actor>();
+        }
+    }
+}
--- .NET/PeliculasAPI/Startup.cs
+++ .NET/PeliculasAPI/Startup.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-// using AutoMapper;
+using AutoMapper;
 // using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -18,7 +18,7 @@ using Microsoft.Extensions.Logging;
 // using Microsoft.IdentityModel.Tokens;
 // using NetTopologySuite;
 // using NetTopologySuite.Geometries;
-// using PeliculasAPI.Helpers;
+using PeliculasAPI.Helpers;
 // using PeliculasAPI.Servicios;
 
 namespace PeliculasAPI
@@ -35,7 +35,7 @@ namespace PeliculasAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // services.AddAutoMapper(typeof(Startup));
+            services.AddAutoMapper(typeof(Startup));
 
             // services.AddTransient<IAlmacenadorArchivos, AlmacenadorArchivosAzure>();
             services.AddHttpContextAccessor();
