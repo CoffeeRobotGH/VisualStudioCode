c337944b92c4a7cf39c10c65e8506b0f31fe18ac
CoffeeRobotGH <emanuelparedesokorov@gmail.com>
2023-09-13 10:44:56 -0300

    Pruebas Unitarias

--- .NET/PeliculasAPI/DTOs/ReviewDTO.cs
+++ .NET/PeliculasAPI/DTOs/ReviewDTO.cs
@@ -1,21 +1,17 @@
-using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PeliculasAPI.Entidades
+namespace PeliculasAPI.DTOs
 {
-    public class Review: IId
+    public class ReviewDTO
     {
         public int Id { get; set; }
         public string Comentario { get; set; }
-        [Range(1, 5)]
         public int Puntuacion { get; set; }
         public int PeliculaId { get; set; }
-        public Pelicula Pelicula { get; set; }
         public string UsuarioId { get; set; }
-        public IdentityUser Usuario { get; set; }
+        public string NombreUsuario { get; set; }
     }
 }
--- .NET/PeliculasAPI/Startup.cs
+++ .NET/PeliculasAPI/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
