aadeace3c8aa13b299540caa019b5ff6103633e1
CoffeeRobotGH <emanuelparedesokorov@gmail.com>
2023-11-09 11:02:59 -0300

    02-Variables

---
+++ JavaScript/Curso JS Moderno/02-Variables/js/01-app.js
@@ -0,0 +1,37 @@
+// Inicializar variable con un valor
+var producto = "Monitor de 24 Pulgadas";
+    console.log(producto);
+
+// Las variables se pueden reasignar
+producto = 'Monitor de 19 Pulgadas';
+    console.log(producto);
+
+// JS es un lenguaje de tipo dinamico, no se especifica tipo de dato
+producto = 20;
+    console.log(producto);
+
+// Se pueden inicializar sin valor y asignarlo despues
+var disponible = true;
+disponible = true;
+
+disponible = false;
+
+// Inicializar múltiples variables
+var categoria =  'Computadoras',
+    marca = "Marca Famosa";
+var calificacion = 5;
+
+// Las variabels no pueden iniciar con números
+/*
+var 99dais;
+var dias99;
+
+var 01_;
+var _01;
+var _usuario;
+var __Usuario
+
+var nombreProducto;
+var nombre_producto_categoria_precio;
+var nombre precio;
+*/
