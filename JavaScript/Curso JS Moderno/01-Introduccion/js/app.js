0504af4fc5a4d75f5fabe35229db1cfdd6629144
CoffeeRobotGH <emanuelparedesokorov@gmail.com>
2023-11-09 10:12:06 -0300

    01-Introducción

---
+++ JavaScript/Curso JS Moderno/01-Introduccion/js/app.js
@@ -0,0 +1,20 @@
+// alert('Hola Mundo');
+
+const nombre = prompt('Cual es tu nombre?');
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo JavaScript Moderno`;
+
+// Pruebas de consola
+/*
+console.time('Hola');
+
+    console.warn('Eso no esta permitido');
+    console.warn('Eso no esta permitido');
+    console.warn('Eso no esta permitido');
+    console.warn('Eso no esta permitido');
+    console.warn('Eso no esta permitido');
+    console.warn('Eso no esta permitido');
+    console.warn('Eso no esta permitido');
+    console.warn('Eso no esta permitido');
+
+console.timeEnd('Hola');
+*/
