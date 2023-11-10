d2d03da696b2bb2ab92583d38f31a8cd7c31a870
CoffeeRobotGH <emanuelparedesokorov@gmail.com>
2023-11-10 10:36:59 -0300

    04-Números

---
+++ JavaScript/Curso JS Moderno/04-Numeros/js/05-app.js
@@ -0,0 +1,22 @@
+let puntaje = 5;
+
+puntaje++; // 5
+console.log(puntaje); // 6
+
+++puntaje; // 7
+console.log(puntaje); // 7
+
+puntaje--; // 7
+console.log(puntaje); // 6
+
+--puntaje; // 5
+console.log(puntaje); //5
+
+
+let puntaje2 = 5;
+
+puntaje2 += 4;
+console.log(puntaje2); // 9
+
+puntaje2 -= 6;
+console.log(puntaje2); // 3
