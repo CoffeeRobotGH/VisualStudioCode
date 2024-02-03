d40490797e98baa04c521dad520f41dde657955d
CoffeeRobotGH <emanuelparedesokorov@gmail.com>
2024-02-03 16:28:34 -0300

    33-Promises

---
+++ JavaScript/Curso JS Moderno/33-Promises/js/03-app.js
@@ -0,0 +1,21 @@
+const aplicarDescuento = new Promise( (resolve, reject) => {
+    const descuento = true;
+
+    if(descuento) {
+        resolve('Descuento Aplicado'); 
+    } else {
+        reject('No se pudo aplicar el descuento');
+    }
+
+});
+
+aplicarDescuento
+    .then( resultado =>  {
+        console.log(resultado);
+    }).catch( error => {
+        console.log(error);
+    });
+
+function descuento(mensaje) {
+    console.log(mensaje);
+}
