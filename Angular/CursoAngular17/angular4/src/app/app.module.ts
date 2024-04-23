dfce0f69450fe1d59176091e743cd83657d247fa
CoffeeRobotGH <emanuelparedesokorov@gmail.com>
2024-04-23 11:37:39 -0300

    Primeros pasos - Componentes y Plantillas

---
+++ Angular/CursoAngular17/angular4/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FrutaComponent } from './fruta/fruta.component';
+import { EmpleadoComponent } from './empleado/empleado.componente';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FrutaComponent,
+    EmpleadoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
